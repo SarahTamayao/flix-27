dfced5a2f9229ff4b8caa624b8386da283c51ce0
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-02-25 19:06:25 -0500

    Finished implementing Networking and fetching data, got json response with 200 code but small error with element expected error that doesn't seem to affect anything

---
+++ app/src/main/java/com/example/flixter/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.flixter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+
+
+// Define tag
+private const val TAG = "MainActivity" // Name of class
+// DB URL Endpoint
+private const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Create new http client
+        val client = AsyncHttpClient()
+        client.get(NOW_PLAYING_URL, object: JsonHttpResponseHandler(){
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e(TAG, "onFailure $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                Log.i(TAG, "onSuccess: JSON data $json")
+                val movieJsonArray = json.jsonObject.getJSONArray("results")
+            }
+
+        })
+    }
+}
