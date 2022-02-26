62e42fc3d691d152510f87b15ffb5c6cce8d4d5f
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-02-26 00:53:08 -0500

    completed project with additional loading image feature

--- app/src/main/java/com/example/flixter/MovieAdapter.kt
+++ app/src/main/java/com/example/flixter/MovieAdapter.kt
@@ -1,6 +1,9 @@
 package com.example.flixter
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.TextureView
 import androidx.recyclerview.widget.RecyclerView
@@ -8,19 +11,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 
+@GlideModule
+class MyAppGlideModule : AppGlideModule() {
+    // Leaving this empty for now
+}
+
+private const val TAG = "MovieAdapter"
 // Create MovieAdapter class constructor that takes a context and a non-mutable list of movies
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
     // Adapter is an abstract class, so these need to be implemented
+    // This is the expensive operation which involves creating a view
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        Log.i(TAG, "onCreateViewHolder")
         val view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
         return ViewHolder(view)
     }
 
     // Given viewholder and position, take the data at the position and bind it into the viewholder
+    // This operation is cheap as you simply bind data to an existing viewholder
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.i(TAG, "onBindViewholder position $position")
         val movie = movies[position]
         holder.bind(movie)
     }
@@ -38,7 +54,10 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
-            // TODO: populate imageView
+            Glide.with(context)
+                .load(movie.posterImageUrl)
+                .placeholder(R.drawable.doge)
+                .into(ivPoster)
         }
     }
 }
