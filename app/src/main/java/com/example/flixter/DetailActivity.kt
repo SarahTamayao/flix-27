3ede6e1191a4e2565fb4d454566b63c4c8878343
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-03-08 21:42:30 -0500

    Setup framework for the youtube player and added generic video to the activity screen

--- app/src/main/java/com/example/flixter/DetailActivity.kt
+++ app/src/main/java/com/example/flixter/DetailActivity.kt
@@ -6,14 +6,21 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.RatingBar
 import android.widget.TextView
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
+private const val YOUTUBE_API_KEY = "AIzaSyD6Y9aoBJblxE0oTd9EKAMQZiQVQPJH-f0"
 private const val TAG = "DetailActivity"
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : YouTubeBaseActivity() {
 
-    // These declare lateinit variables for the screen components
+    // These declare lateinit variables for the screen components to have references to them
     private lateinit var tvTitle: TextView
     private lateinit var tvOverview: TextView
     private lateinit var ratingBar: RatingBar
+
+    private lateinit var ytPlayerView: YouTubePlayerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +29,7 @@ class DetailActivity : AppCompatActivity() {
         tvTitle = findViewById(R.id.tvTitle)
         tvOverview = findViewById(R.id.tvOverview)
         ratingBar = findViewById(R.id.rbVoteAverage)
+        ytPlayerView = findViewById(R.id.player) // youtube playerview
 
         val movie = intent.getParcelableExtra<Movie>(MOVIE_EXTRA) as Movie
         Log.i(TAG, "Movie is $movie")
@@ -31,5 +39,24 @@ class DetailActivity : AppCompatActivity() {
         tvOverview.text = movie.overview
         // Get the rating of the movie
         ratingBar.rating = movie.voteAverage.toFloat() // Downcast since rating takes a float, but we give it a double
+
+        ytPlayerView.initialize(YOUTUBE_API_KEY, object: YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider?,
+                player: YouTubePlayer?,
+                p2: Boolean
+            ) {
+                Log.i(TAG, "onInitializationSuccess")
+                player?.cueVideo("5xVh-7ywKpE")
+            }
+
+            override fun onInitializationFailure(
+                p0: YouTubePlayer.Provider?,
+                p1: YouTubeInitializationResult?
+            ) {
+                Log.i(TAG, "onInitializationFailure")
+            }
+
+        })
     }
 }
